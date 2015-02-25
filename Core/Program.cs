f08a95ced3acea015011976c805afc23fd29adee
Joshua Zenn <wordman05@hotmail.com>
2015-02-25 11:32:01 -0500

    Added Core project

---
+++ Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up...");
+            //Insert startup args + variables
+        }
+    }
+}
