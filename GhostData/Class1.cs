290d5316298a15ab1eebe1472e9a6b4d3aa8c00a
ARMmaster17 <wordman05@gmail.com>
2015-03-03 15:39:05 -0500

    Initial commit of GhostData

---
+++ GhostData/Class1.cs
@@ -0,0 +1,34 @@
+﻿//////////////////////////////////////////////
+// Copyright Joshua Zenn 2015               //
+// Protected by propietary licensing. See   //
+// https://github.com/ARMmaster17/GhostData //
+// for more details and to obtain           //
+// sublicensing for your project            //
+//////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostData
+{
+    public class GhostDataXML : GhostDataGeneric
+    {
+        public GhostDataXML()
+        {
+
+        }
+    }
+    public class GhostDataSQL : GhostDataGeneric
+    {
+        public GhostDataSQL()
+        {
+
+        }
+    }
+    public class GhostDataGeneric
+    {
+
+    }
+}
