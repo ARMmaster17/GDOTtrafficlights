09ebe44c79607f4cf5587761731792ede517a689
Joshua Zenn <wordman05@gmail.com>
2015-03-17 09:41:57 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ mkdir latest
 # DEBUG
 ls
 # COPY FILES
-cp /LICENSE /latest/LICENSE
-cp /README.md /latest/README.md
-cp /settings.ini /latest/settings.ini
-cp /modules.ini /latest/modules.ini
-cp /Core/bin/Release/Core.exe /latest/Core.exe
-cd /latest
+cp ./LICENSE ./latest/LICENSE
+cp ./README.md ./latest/README.md
+cp ./settings.ini ./latest/settings.ini
+cp ./modules.ini ./latest/modules.ini
+cp ./Core/bin/Release/Core.exe ./latest/Core.exe
+cd ./latest
 mkdir TLgui
 cd ..
-cp /TLgui/bin/Release/TLgui.exe /latest/TLgui/TLgui.exe
+cp ./TLgui/bin/Release/TLgui.exe ./latest/TLgui/TLgui.exe
