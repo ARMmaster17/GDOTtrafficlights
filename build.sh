7c5db093d098b1c7879f735514519b2a037a6705
Joshua Zenn <wordman05@gmail.com>
2015-03-17 09:50:32 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,12 +6,25 @@ mkdir latest
 # DEBUG
 ls
 # COPY FILES
+echo 'Copying LICENSE'
 cp ./LICENSE ./latest/LICENSE
+echo 'Copying README.md'
 cp ./README.md ./latest/README.md
+echo 'Copying settings.ini'
 cp ./settings.ini ./latest/settings.ini
+echo 'Copying modules.ini'
 cp ./modules.ini ./latest/modules.ini
+echo 'Copying Core.exe'
 cp ./Core/bin/Release/Core.exe ./latest/Core.exe
+echo 'Copying TLgui'
 cd ./latest
 mkdir TLgui
 cd ..
 cp ./TLgui/bin/Release/TLgui.exe ./latest/TLgui/TLgui.exe
+echo 'Copying TLpserver'
+cd ./latest
+mkdir TLpserver
+cd ..
+cp ./TLpserver/bin/Release/TLpserver.exe ./latest/TLserver/TLserver.exe
+
+tar -czf latest.tar.gz ./latest
